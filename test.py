a20d91d71a38eadfdf0d96bbbb2a9080ae116e3b
Julian <julian-pyrlik@gmx.de>
2024-03-07 18:54:07 +0100

    Delete test

--- test.py
+++
@@ -1,6 +0,0 @@
-userdata = [{'stock': 'nflx', 'amount': 10, 'stock_price': 572.29}, {'stock': 'nflx', 'amount': 1, 'stock_price': 572.21}]
-
-for element in userdata:
-    element['test'] = 5
-
-print(userdata)
